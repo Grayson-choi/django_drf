02e39853267407afbd778e32d90f3e37a9874f9e
Grayson-choi <784wldnd@gmail.com>
2021-04-17 20:27:32 +0900

    html vs json 응답 확인하기

--- articles/urls.py
+++ articles/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from . import path
+from . import views
 
 urlpatterns = [
-    
+    path('html/', views.article_html, name='article_html'),
+    path('json-1/', views.article_json_1, name='json_1'),
 ]
--- articles/views.py
+++ articles/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_list_or_404
+from django.http.response import JsonResponse
+# 써드파티
+from .models import Article # import 순서 강조해주기
 
 # Create your views here.
+def article_html(request):
+    articles = get_list_or_404(Article) # article이 하나도 없다면 404 에러를 띄워준다. 여러개라 get_list_or_404를 써야함
+    context = {
+        'articles': articles
+    }
+    return render(request, 'articles/article.html', context)
+
+
+def article_json_1(request):
+    articles = get_list_or_404(Article)
+    articles_json = []
+
+    for article in articles:
+        articles_json.append(
+            {
+                'id': article.pk,
+                'title': article.title,
+                'content': article.content,
+                'created_at': article.created_at,
+                'updated_at': article.updated_at,
+            }
+        )
+    return JsonResponse(articles_json, safe=False) # False로 설정하면 모든 개체를 serialization 할 수 있음 (그렇지 않으면 dict 인스턴스 만 허용됨)
